fa64f68d2e934538e67c9aa89cb55580972ace59
jasonrdonald <jasonrdonald@yahoo.co.in>
2021-06-15 01:23:56 +0530

    Update App.js

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -1,18 +1,39 @@
 import './App.css';
 import React from 'react';
-import Navigation from './Navigation';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import TasksPage from "./pages/navtasks";
 import ConfigPage from "./pages/navconfig";
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <TasksPage />
-      <ConfigPage />
-    </div>
+    <Router>
+    <Container>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Tasks</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/config">Config</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <hr />
+
+        <Route exact path="/" component={TasksPage} />
+        <Route path="/config" component={ConfigPage} />
+      </Container>
+  </Router>
   );
 }
 
